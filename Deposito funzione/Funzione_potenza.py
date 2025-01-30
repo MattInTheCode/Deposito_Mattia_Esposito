40746f2572a9de582aa1b45fb61d4d532ae9ed98
MattInTheCode <esposito0mattia@gmail.com>
2025-01-30 11:13:09 +0100

    Aggiunta cartella "deposito funzioni"
    all'interno della cartella troviamo come primo funzione potenza che permette di calcolare fino alla  quinta.

---
+++ Deposito funzione/Funzione_potenza.py
@@ -0,0 +1,30 @@
+
+def quadrato(numero):
+    return numero * numero 
+
+num = int(input("Inserisci un numero che vuoi al quadrato:"))
+risultato1 = quadrato(num)
+print(risultato1)
+
+
+def cubo(n):
+    return n * n * n
+
+num = int(input("Inserisci un numero che vuoi al cubo:"))
+risultato2= cubo(num)
+print(risultato2)
+
+def quarta(n):
+    return n * n * n * n
+
+num = int(input("Inserisci un numero che vuoi alla quarta:"))
+risultato3= quarta(num)
+print(risultato3)
+
+
+def quinta(n):
+    return n * n * n * n * n 
+
+num = int(input("inserisci un numero che vuoi alla quinta:"))
+risultato4= quinta(num)
+print(risultato4)
